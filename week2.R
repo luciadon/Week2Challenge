3f0d6fa00e75c17c5927959068bcf9eb19b45d6c
Donskoi <don19q@csiro.au>
2020-03-17 13:36:40 +1000

    Fixed Q2 on interpretation, complete Q3

--- week2.R
+++ week2.R
@@ -22,7 +22,19 @@ filterered2
 ?as.numeric
 bom_clean <- mutate(filterered2, Temp_minnum = (as.numeric(filterered2$Temp_min)), Temp_maxnum = (as.numeric(filterered2$Temp_max))) %>% 
   mutate(Temp_diff = Temp_maxnum-Temp_minnum)
-Question_2 <- bom_clean %>% group_by(Year, Month) %>% summarise(avg_daily = mean(Temp_diff))%>% arrange(avg_daily)
-Question_2                                                  
-#March 1980 saw the lowest average daily temperature difference#
+Question_2 <- bom_clean %>% group_by(Month) %>% summarise(avg_daily = mean(Temp_diff))%>% arrange(avg_daily)
+Question_2 %>% head(1)                                                
+# June saw the lowest average daily temperature difference#
 
+#answering question 3#
+tidy_bom <- gather(BOM_stations, key = Station_num , value = value, -info) %>% 
+spread(key=info, value = value)
+num_bom <- mutate(tidy_bom, Station_number = (as.numeric(tidy_bom$Station_num)))
+num_bom
+Bom_seperate
+joined <- left_join (bom_clean, num_bom, by = c('Station_number'='Station_number'))
+joined
+Question_3 <- joined %>% group_by(state) %>% summarise(avg_daily= mean(Temp_diff)) %>% arrange(avg_daily) %>% head(1)
+Question_3
+
+#answering question 4#
